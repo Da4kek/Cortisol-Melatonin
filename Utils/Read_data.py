2ac70e58acc1256442e37016028fe863e5b7e13d
Dark <anisri477@gmail.com>
2024-12-08 22:00:35 +0530

    utils init

---
+++ Utils/Read_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np 
+
+
+def create_dataset(users, file_name, replace_na=True):
+    df_concat = pd.DataFrame()
+    for user in users:
+        try:
+            df = pd.read_csv('../DataPaper/%s/%s.csv'%(user,file_name))
+            df['user'] = user
+            df_concat = pd.concat([df_concat,df])
+        except:
+            print('NO data for %s'%user)
+            pass
+    
+    del df_concat['Unnamed: 0']
+    df_concat = df_concat.set_index('user')
+
+    if replace_na == True:
+        df_concat = df_concat.replace(0,np.nan)
+
+    return(df_concat)
