8bd91088abf2c4243662b19d0f5894bd78877993
Dark <anisri477@gmail.com>
2024-12-10 15:38:38 +0530

    added eda

--- Utils/Read_data.py
+++ Utils/Read_data.py
@@ -6,17 +6,18 @@ def create_dataset(users, file_name, replace_na=True):
     df_concat = pd.DataFrame()
     for user in users:
         try:
-            df = pd.read_csv('../DataPaper/%s/%s.csv'%(user,file_name))
+            df = pd.read_csv('DataPaper/%s/%s.csv' %
+                                 (user, file_name))
             df['user'] = user
-            df_concat = pd.concat([df_concat,df])
+            df_concat = pd.concat([df_concat, df])
         except:
-            print('NO data for %s'%user)
+            print('NO data for %s' % user)
             pass
-    
+
     del df_concat['Unnamed: 0']
-    df_concat = df_concat.set_index('user')
+    # df_concat = df_concat.set_index('user')
 
     if replace_na == True:
-        df_concat = df_concat.replace(0,np.nan)
+        df_concat = df_concat.replace(0, np.nan)
 
-    return(df_concat)
+    return (df_concat)
